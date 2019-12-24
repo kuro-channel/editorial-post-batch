efac24e1ddf58fd8f5d7505043279c59826a92d0
kuro-channel <takumikuroiwa@gmail.com>
2019-12-25 00:22:27 +0900

    src commit

---
+++ editorial-post/src/main/java/jp/kurochanel/batch/editorialpost/EditorialPostApplication.java
@@ -0,0 +1,13 @@
+package jp.kurochanel.batch.editorialpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorialPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorialPostApplication.class, args);
+    }
+
+}
---
+++ editorial-post/src/test/java/jp/kurochanel/batch/editorialpost/EditorialPostApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kurochanel.batch.editorialpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorialPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
